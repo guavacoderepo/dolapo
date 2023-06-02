b6490c2340cd8613d810122384894122ea672248
guavacoderepo <guavacodeplus@gmail.com>
2023-06-02 11:56:34 +0100

    update

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color appColor = Colors.black;
+Color light = Colors.white;
+Color blue = const Color(0xff33BDFE);
+Color grey = const Color(0xffD9D9D9);
+
---
+++ lib/constants/fonts.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+// font 100
+h100(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w100,
+        color: color,
+      ),
+    );
+
+// font 200
+h200(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w200,
+        color: color,
+      ),
+    );
+
+// font 300
+h300(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w300,
+        color: color,
+      ),
+    );
+
+// font 400
+h400(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+        color: color,
+      ),
+    );
+
+// font 500
+h500(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w500,
+        color: color,
+      ),
+    );
+
+// font 600
+h600(text, double size,
+        {Color color = const Color(0xffD9D9D9),
+        TextAlign align = TextAlign.left}) =>
+    Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w600,
+        color: color,
+      ),
+    );
